fea51cbf9128854b1720954080cd901a8190a562
someone <someone@someplace.com>
2024-09-09 11:37:16 +0200

    hero1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Hero } from './heroes/hero';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'tour-of-heroes';
